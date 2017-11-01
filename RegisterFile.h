029eacaa2c6f93d4ccc8f4249871647ee246cb51
Are Oelsner <are.oelsner@richmond.edu>
2017-11-01 12:37:57 -0400

    Added RegisterFile.h prototype of register file

---
+++ RegisterFile.h
@@ -0,0 +1,35 @@
+/* Author: Are Oelsner
+ * Register File
+ * Reads in input registers and or immediate, outputs the relevant data to AL
+ */
+#ifndef __REGISTERFILE_H__
+#define __REGISTERFILE_H__
+
+#include"RegisterTable.h"
+
+class RegisterFile {
+  protected:
+    RegisterTable::RegisterTable registerTable;
+
+  public:
+    ///Constructors
+    //Default Constructor
+    RegisterFile();
+
+    ///Functions
+    //Read Register 1 -- rs
+    RegisterTable::Register readRegister1(RegisterTable::Register r1);
+    //Read Register 2 -- rt
+    RegisterTable::Register readRegister2(RegisterTable::Register r2);
+    //Read Data 1 -- goes to ALU
+    
+    //Read Data 2 -- goes to Mux or  Write data of Data memory
+
+    //Write Register -- received from Mux  of rd or rt
+    void writeRegister(RegisterTable::Register rd, string binaryValue);
+    //Write Data
+    void writeData(RegisterTable::Register 
+
+};
+
+#endif
