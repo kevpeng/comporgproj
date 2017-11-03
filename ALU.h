e9c49e0d27ad033678fcbd335c8115fc76e6714c
ty3gx <tianchang.yang@richmond.edu>
2017-11-03 15:29:27 -0400

    Completed ALU function
    Completed the ALU implementation, added tester, added comments.

--- ALU.h
+++ ALU.h
@@ -6,12 +6,14 @@
 *  method isEqual). 
 *
 *  Author: Tianchang Yang
+*  Date: 11/3/17
 */
 
 #ifndef ALU_h
 #define ALU_h
 
 #include <string>
+#include <iostream>
 
 using namespace std;
 class ALU {
@@ -24,20 +26,34 @@ public:
 	//performs appropriate function
 	void execute();
 
-	//Set operand1 equal to the argument string
+	//Set operand1 equal to the argument string - input format must be a 32 bits binary string
 	void setOperand1(string operand);
 
-	//Set operand2 equal to the argument string
+	//Set operand2 equal to the argument string - input format must be a 32 bits binary string
 	void setOperand2(string operand);
 
-	//Sets the operation equal to the argument int
-	void setOperation(int in);
+	//Sets the operation equal to the argument int 
+	//compare is 0; addition is 1; subtract is 2;
+	void setOperation(int operation);
 
-	//Returns the output
+	//Returns the output - format is 32 bits binary string
+	//except when comparing - then it would be string "true" if they are equal, "false" otherwise
 	string getOutput();
 
-	//returns true if two operands are equal, false otherwise
-	bool isEqual();
+	//returns string "true" if two operands are equal, string "false" otherwise
+	string isEqual();
+
+private:
+	int myOperation; // compare is 0; addition is 1; subtract is 2;
+	string myOperand1; //32 bits binary string
+	string myOperand2; //32 bits binary string
+	string myOutput; //32 bits result string
+
+	//helper methods
+	void compare();
+	void add();
+	string add(string operand1, string operand2);
+	void subtract();
 };
 
 #endif
