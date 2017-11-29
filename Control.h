0b4e432029e259e5e7a3b5592f8a25e3dfc91245
areo279 <are.oelsner@richmond.edu>
2017-11-29 12:40:22 -0500

    Are -updated Control

--- Control.h
+++ Control.h
@@ -11,18 +11,18 @@ class Control {
   private:
     ///Private Variables
     // Input
-    string opcode; //six bit instruction opcode
-    string func; //six bit instruction func field
+    string opcode; //six bit instruction opcode from Instruction Memory
+    string func; //six bit instruction func field from Instruction Memory
     string zero; //zero output from ALU
 
     // Output
-    string RegDst; //Bool? int?
-    string RegWrite;
-    string ALUSrc;
-    string ALUOp;
-    string MemWrite;
-    string MemRead;
-    string MemToReg;
+    string RegDst; //Bool? int? Determines if a Register rd is used?
+    string RegWrite; // Determines if Register is written 
+    string ALUSrc; // Determines value going into ALU
+    string ALUOp; // Determines operation of ALU
+    string MemWrite; // Determines if Memory is written to
+    string MemRead; // Determines if memory is read
+    string MemToReg; // Determines if Memory data is written to a register
 
   public:
     ///Constructors
