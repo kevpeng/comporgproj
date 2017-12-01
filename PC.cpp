80a3bf06074a0aa826addd53dd582ade000aec8b
Kevin Peng <kevinc.peng@gmail.com>
2017-12-01 14:33:25 -0500

    Add files via upload

--- PC.cpp
+++ PC.cpp
@@ -7,19 +7,24 @@
 
 #include "PC.h" 
 #include "ALU.h"
-
+/*
 void PC::updateAddress()
 {
 	string binaryAddress = PC::hex2Bin(currentAddress);
-	currentAddress = ALU::add(binaryAddress, "00000000000000000000000000000100");	
-	/*
- 	* need to convert it back to hex... 
- 	*/ 
+	ALU temp = ALU();
+	temp.setOperand1(binaryAddress);
+	temp.setOperand2("00000000000000000000000000000100");
+	temp.setOperation(1);
+	temp.execute();
+	currentAddress = temp.getOutput(); 
+	// currentAddress = bin2Hex(currentAddress);
 }
 
 string PC::hex2Bin(string s)
 {
-  	string result;
+  	if(s.length() == 32) { return s; }
+
+	string result;
   	for (int i = 0; i < s.length(); i++) {
     	char c = s[i];
    		if (c == '0')  result += "0000";
@@ -43,5 +48,44 @@ string PC::hex2Bin(string s)
 }
 
 string PC::bin2Hex(string s) 
-{
+{	// code taken from stackoverflow.com/questions/29210120/binary-string-to-
+	// hex-c 
+	string binToHex = "";
+	for(int i = 0; i < (s.size() - 1); i++){
+    	string s;
+		string tmp = "";
+    	for (int j = 0; j < s.size(); j += 4){
+    	    tmp = s.substr(j, 4);
+    	    if      (!tmp.compare("0000")) binToHex += "0";
+        	else if (!tmp.compare("0001")) binToHex += "1";
+        	else if (!tmp.compare("0010")) binToHex += "2";
+       		else if (!tmp.compare("0011")) binToHex += "3";
+       	 	else if (!tmp.compare("0100")) binToHex += "4";
+       	 	else if (!tmp.compare("0101")) binToHex += "5";
+       	 	else if (!tmp.compare("0110")) binToHex += "6";
+       	 	else if (!tmp.compare("0111")) binToHex += "7";
+       	 	else if (!tmp.compare("1000")) binToHex += "8";
+       	 	else if (!tmp.compare("1001")) binToHex += "9";
+       	 	else if (!tmp.compare("1010")) binToHex += "A";
+       	 	else if (!tmp.compare("1011")) binToHex += "B";
+       	 	else if (!tmp.compare("1100")) binToHex += "C";
+       	 	else if (!tmp.compare("1101")) binToHex += "D";
+       	 	else if (!tmp.compare("1110")) binToHex += "E";
+       	 	else if (!tmp.compare("1111")) binToHex += "F";
+       	 	else continue;
+		cout << "binToHex = " << binToHex << endl;
+    	}
+		cout << "binToHex = " << binToHex << endl;
+    	return binToHex;
+	}
+	*/
+/*	string out;
+	int len = s.length();
+	int temp;
+	for(int i(0); i + 1 < len; i += 4){
+		if(stringstream(s.substr(i,4)) >> hex >> temp){
+		out.push_back(temp);
+		}
+	}
+*/
 }
